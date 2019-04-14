d2fb1c0477ed37be112819251fd10434ebd865a8
Victor Pikula <victor@pikula.com>
2019-04-14 20:48:19 +0200

    First commit

---
+++ TeslaDashcamPlayer/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace TeslaDashcamPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayerLeft = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayerFront = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayerRight = new AxWMPLib.AxWindowsMediaPlayer();
+            this.currentStateLabel = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerFront)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayerLeft
+            // 
+            this.axWindowsMediaPlayerLeft.Enabled = true;
+            this.axWindowsMediaPlayerLeft.Location = new System.Drawing.Point(340, 42);
+            this.axWindowsMediaPlayerLeft.Name = "axWindowsMediaPlayerLeft";
+            this.axWindowsMediaPlayerLeft.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayerLeft.OcxState")));
+            this.axWindowsMediaPlayerLeft.Size = new System.Drawing.Size(640, 480);
+            this.axWindowsMediaPlayerLeft.TabIndex = 0;
+            // 
+            // axWindowsMediaPlayerFront
+            // 
+            this.axWindowsMediaPlayerFront.Enabled = true;
+            this.axWindowsMediaPlayerFront.Location = new System.Drawing.Point(980, 42);
+            this.axWindowsMediaPlayerFront.Name = "axWindowsMediaPlayerFront";
+            this.axWindowsMediaPlayerFront.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayerFront.OcxState")));
+            this.axWindowsMediaPlayerFront.Size = new System.Drawing.Size(640, 480);
+            this.axWindowsMediaPlayerFront.TabIndex = 3;
+            this.axWindowsMediaPlayerFront.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer2_PlayStateChange);
+            // 
+            // axWindowsMediaPlayerRight
+            // 
+            this.axWindowsMediaPlayerRight.Enabled = true;
+            this.axWindowsMediaPlayerRight.Location = new System.Drawing.Point(1620, 42);
+            this.axWindowsMediaPlayerRight.Name = "axWindowsMediaPlayerRight";
+            this.axWindowsMediaPlayerRight.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayerRight.OcxState")));
+            this.axWindowsMediaPlayerRight.Size = new System.Drawing.Size(640, 480);
+            this.axWindowsMediaPlayerRight.TabIndex = 4;
+            // 
+            // currentStateLabel
+            // 
+            this.currentStateLabel.Location = new System.Drawing.Point(794, 16);
+            this.currentStateLabel.Name = "currentStateLabel";
+            this.currentStateLabel.Size = new System.Drawing.Size(693, 20);
+            this.currentStateLabel.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 212);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Recent Clips:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Saved Clips:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(16, 306);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(150, 212);
+            this.listBox2.TabIndex = 9;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(184, 306);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(150, 212);
+            this.listBox3.TabIndex = 10;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(147, 21);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2272, 530);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.currentStateLabel);
+            this.Controls.Add(this.axWindowsMediaPlayerRight);
+            this.Controls.Add(this.axWindowsMediaPlayerFront);
+            this.Controls.Add(this.axWindowsMediaPlayerLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerFront)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerRight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayerLeft;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayerFront;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayerRight;
+        private System.Windows.Forms.TextBox currentStateLabel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TeslaDashcamPlayer/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeslaDashcamPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                comboBox1.Items.Add(drive.Name);
+
+                if (comboBox1.SelectedIndex == -1 && drive.DriveType == DriveType.Removable)
+                {
+                    comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+                }
+            }
+
+        }
+
+        private void LoadItems()
+        {
+            listBox1.Items.Clear();
+            DirectoryInfo dinfo = new DirectoryInfo(comboBox1.SelectedItem + "TeslaCam\\RecentClips");
+            if (dinfo.Exists)
+            {
+                FileInfo[] files = dinfo.GetFiles("*-front.mp4");
+                foreach (FileInfo file in files)
+                {
+                    listBox1.Items.Add(file.Name.Replace("-front.mp4", ""));
+                }
+
+                listBox2.Items.Clear();
+                dinfo = new DirectoryInfo(comboBox1.SelectedItem + "TeslaCam\\SavedClips");
+                var directories = dinfo.GetDirectories();
+                foreach (DirectoryInfo directory in directories)
+                {
+                    listBox2.Items.Add(directory.Name);
+                }
+            }
+        }
+
+        private void axWindowsMediaPlayer2_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            switch (e.newState)
+            {
+                case 0:    // Undefined
+                    currentStateLabel.Text = "Undefined";
+                    break;
+
+                case 1:    // Stopped
+                    currentStateLabel.Text = "Stopped";
+                    axWindowsMediaPlayerLeft.Ctlcontrols.stop();
+                    axWindowsMediaPlayerRight.Ctlcontrols.stop();
+                    break;
+
+                case 2:    // Paused
+                    currentStateLabel.Text = "Paused";
+                    axWindowsMediaPlayerLeft.Ctlcontrols.pause();
+                    axWindowsMediaPlayerRight.Ctlcontrols.pause();
+                    break;
+
+                case 3:    // Playing
+                    currentStateLabel.Text = "Playing";
+                    axWindowsMediaPlayerLeft.Ctlcontrols.play();
+                    axWindowsMediaPlayerRight.Ctlcontrols.play();
+                    break;
+
+                case 4:    // ScanForward
+                    currentStateLabel.Text = "ScanForward";
+                    break;
+
+                case 5:    // ScanReverse
+                    currentStateLabel.Text = "ScanReverse";
+                    break;
+
+                case 6:    // Buffering
+                    currentStateLabel.Text = "Buffering";
+                    break;
+
+                case 7:    // Waiting
+                    currentStateLabel.Text = "Waiting";
+                    break;
+
+                case 8:    // MediaEnded
+                    currentStateLabel.Text = "MediaEnded";
+                    break;
+
+                case 9:    // Transitioning
+                    currentStateLabel.Text = "Transitioning";
+                    break;
+
+                case 10:   // Ready
+                    currentStateLabel.Text = "Ready";
+                    break;
+
+                case 11:   // Reconnecting
+                    currentStateLabel.Text = "Reconnecting";
+                    break;
+
+                case 12:   // Last
+                    currentStateLabel.Text = "Last";
+                    break;
+
+                default:
+                    currentStateLabel.Text = ("Unknown State: " + e.newState.ToString());
+                    break;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = listBox1.SelectedItem.ToString();
+            axWindowsMediaPlayerFront.URL = comboBox1.SelectedItem + "TeslaCam\\RecentClips\\" + selectedItem + "-front.mp4";
+            axWindowsMediaPlayerLeft.URL = comboBox1.SelectedItem + "TeslaCam\\RecentClips\\" + selectedItem + "-left_repeater.mp4";
+            axWindowsMediaPlayerRight.URL = comboBox1.SelectedItem + "TeslaCam\\RecentClips\\" + selectedItem + "-right_repeater.mp4";
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            string selectedItem = listBox2.SelectedItem.ToString();
+            DirectoryInfo dinfo = new DirectoryInfo(comboBox1.SelectedItem + "TeslaCam\\SavedClips\\" + selectedItem);
+            FileInfo[] files = dinfo.GetFiles("*-front.mp4");
+            foreach (FileInfo file in files)
+            {
+                listBox3.Items.Add(file.Name.Replace("-front.mp4", ""));
+            }
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedFolder = listBox2.SelectedItem.ToString();
+            string selectedItem = listBox3.SelectedItem.ToString();
+            axWindowsMediaPlayerFront.URL = comboBox1.SelectedItem + "TeslaCam\\SavedClips\\" + selectedFolder + "\\" +  selectedItem + "-front.mp4";
+            axWindowsMediaPlayerLeft.URL = comboBox1.SelectedItem + "TeslaCam\\SavedClips\\" + selectedFolder + "\\" + selectedItem + "-left_repeater.mp4";
+            axWindowsMediaPlayerRight.URL = comboBox1.SelectedItem + "TeslaCam\\SavedClips\\" + selectedFolder + "\\" + selectedItem + "-right_repeater.mp4";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadItems();
+        }
+    }
+}
